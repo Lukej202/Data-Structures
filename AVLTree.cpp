62dc8083847d8170886c131618c7cf653ba5e22c
Lukej202 <122322055+Lukej202@users.noreply.github.com>
2025-03-27 13:53:25 -0400

    Add files via upload

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -10,26 +10,27 @@ AVLTree::AVLTree() {
 }
 
 bool AVLTree::insert(const string &key, int value) {
-    AVLNode* newNode = new AVLNode(key, value, 0);
+    if (this->contains(key) == true) {
+        return false;
+    }
+    AVLNode* newNode = new AVLNode(key, value);
     if (root == nullptr) {
         root = newNode;
+        treeSize++;
+        treeHeight++;
         return true;
     }
     insertHelper(root, newNode);
+    treeHeight = root->findHeight(root);
+    root->getBalance(root);
+    treeSize++;
     return true;
 }
 
-void AVLTree::insertHelper(AVLNode *current, AVLNode *insertNode) {
-    if (insertNode->key < current->key) {
-        if (current->left == nullptr) {
-            current->left = insertNode;
-            return;
-        }
-    } else if (insertNode->key > current->key) {
-        if (current->right == nullptr) {
-            current->right = insertNode;
-            return;
-        }
+void AVLTree::insertHelper(AVLNode *&current, AVLNode *insertNode) {
+    if (current == nullptr) {
+        current = insertNode;
+        return;
     }
     if (insertNode->key < current->key) {
         insertHelper(current->left, insertNode);
@@ -39,25 +40,153 @@ void AVLTree::insertHelper(AVLNode *current, AVLNode *insertNode) {
     }
 }
 
-bool AVLTree::contains(string &key) const {
+bool AVLTree::contains(const string &key) const {
     return containsHelper(root, key);
 }
 
-bool AVLTree::containsHelper(const AVLNode *node, string& key) const {
+bool AVLTree::containsHelper(AVLNode *node, const string& key) const {
     if (node == nullptr) {
         return false;
     }
-    if (node->key == key) {
+    if (key == node->key) {
         return true;
     }
     if (key < node->key) {
-        containsHelper(node->left, key);
+        return containsHelper(node->left, key);
     }
     if (key > node->key) {
-        containsHelper(node->right, key);
+        return containsHelper(node->right, key);
     }
+
+}
+optional<int> AVLTree::get(const string &key) const {
+    return getHelper(root, key);
+}
+
+optional<int> AVLTree::getHelper(AVLNode *node, const string& key) const {
+    if (node == nullptr) {
+        return nullopt;
+    }
+    if (key == node->key) {
+        return node->value;
+    }
+    if (key < node->key) {
+        return getHelper(node->left, key);
+    }
+    if (key > node->key) {
+        return getHelper(node->right, key);
+    }
+}
+
+bool AVLTree::remove(const string &key) {
+    bool removed = removeHelper(root, key);
+    treeHeight = root->findHeight(root);
+    return removed;
+}
+
+bool AVLTree::removeHelper(AVLNode *&node, const string& key) {
+    if (node == nullptr) {
+        return false;
+    }
+    if (key == node->key) {
+        if (node->left == nullptr && node->right == nullptr) {
+            node = nullptr;
+        }
+        else if (node->left != nullptr && node->right == nullptr) {
+            node->key = node->left->key;
+            node->value = node->left->value;
+            node->left = nullptr;
+        }
+        else if (node->left == nullptr && node->right != nullptr) {
+            node->key = node->right->key;
+            node->value = node->right->value;
+            node->right = nullptr;
+        }
+        else{
+            AVLNode *parent = node;
+            AVLNode *successor = node->right;
+            while (successor->left != nullptr) {
+                parent = successor;
+                successor = successor->left;
+            }
+            node->key = successor->key;
+            node->value = successor->value;
+            delete successor;
+            parent->left = nullptr;
+        }
+        treeSize--;
+        return true;
+    }
+    if (key < node->key) {
+        return removeHelper(node->left, key);
+    }
+    if (key > node->key) {
+        return removeHelper(node->right, key);
+    }
+}
+
+int &AVLTree::operator[](const string &key) {
+    return overLoadOneHelper(key,root);
+}
+
+int &AVLTree::overLoadOneHelper(const string &key, AVLNode *node) {
+    if (node == nullptr) {
+        return root->value;
+    }
+    if (key == node->key) {
+        return node->value;
+    }
+    if (key < node->key) {
+        return overLoadOneHelper(key, node->left);
+    }
+    if (key > node->key) {
+        return overLoadOneHelper(key, node->right);
+    }
+}
+
+vector<string> AVLTree::findRange(string lowKey, string highKey) const {
+    AVLNodeVectorVisit v;
+    findRangeHelper(root, v, lowKey, highKey);
+
+    return v.keys;
+}
+
+void AVLTree::findRangeHelper(AVLNode *node, AVLNodeVisitor& visitor, string lowKey, string highKey) const {
+    if (node == nullptr) {
+        return;
+    }
+
+    findRangeHelper(node->left, visitor, lowKey, highKey);
+    if (node->key >= lowKey && node->key <= highKey) {
+        visitor.visit(node);
+    }
+    findRangeHelper(node->right, visitor, lowKey, highKey);
 }
 
 
 
+vector<string> AVLTree::keys() const {
+    AVLNodeVectorVisit v;
+    keysHelper(root, v);
 
+    return v.keys;
+}
+
+void AVLTree::keysHelper(AVLNode *node, AVLNodeVisitor& visitor) const {
+    if (node == nullptr) {
+        return;
+    }
+
+    keysHelper(node->left, visitor);
+    visitor.visit(node);
+    keysHelper(node->right, visitor);
+
+}
+
+size_t AVLTree::size() const {
+    return treeSize;
+}
+
+size_t AVLTree::getHeight() const {
+    return treeHeight;
+}
--- AVLTree.h
+++ AVLTree.h
@@ -4,24 +4,30 @@
 
 #ifndef AVLTREE_H
 #define AVLTREE_H
+#include <iostream>
 #include <optional>
 #include <string>
 #include <vector>
 
+
 using namespace std;
+
 
 class AVLNode {
 public:
     string key;
     int value;
     int height;
+    int balance;
+    int leftHeight;
+    int rightHeight;
     AVLNode *left;
     AVLNode *right;
 
-    AVLNode(string k, int v, int h) {
+    AVLNode(string k, int v) {
         key = k;
         value = v;
-        height = h;
+        height = 0;
         left = nullptr;
         right = nullptr;
 
@@ -38,11 +44,40 @@ public:
         height = h;
     }
 
-    int findHeight(AVLNode *node);
+    int findHeight(AVLNode *node) {
+        if (node == nullptr) {
+            this->height = -1;
+            return -1;
+        }else {
+            int leftHeight = findHeight(node->left);
+            int rightHeight = findHeight(node->right);
+            node->height = max(leftHeight, rightHeight) + 1;
+            node->leftHeight = leftHeight;
+            node->rightHeight = rightHeight;
+            return 1 + max(leftHeight, rightHeight);
+        }
+    }
+
 
     int numChildren(AVLNode *node);
 
-    int getBalance(AVLNode *node);
+    int getBalance(AVLNode *node) {
+        balance = node->leftHeight - node->rightHeight;
+        return balance;
+    }
+};
+
+class AVLNodeVisitor {
+public:
+    virtual void visit(AVLNode *node) = 0;
+};
+
+class AVLNodeVectorVisit : public AVLNodeVisitor {
+    public:
+    vector<string> keys;
+    void visit(AVLNode *node) override{
+        keys.push_back(node->key);
+    }
 };
 
 class AVLTree {
@@ -50,27 +85,41 @@ class AVLTree {
 public:
     AVLNode *root;
 
+    int treeSize = 0;
+
+    int treeHeight = -1;
+
     AVLTree();
 
     //~AVLTree();
 
     bool insert(const string& key, int value);
 
-    void insertHelper(AVLNode* current, AVLNode* insertNode);
+    void insertHelper(AVLNode*& current, AVLNode* insertNode);
 
     bool remove(const string& key);
 
-    bool contains(string &key) const;
+    bool removeHelper(AVLNode*& current, const string& key);
 
-    bool containsHelper(const AVLNode *node, string &key) const;
+    bool contains(const string &key) const;
+
+    bool containsHelper(AVLNode *node, const string &key) const;
 
     optional<int> get(const string& key) const;
 
+    optional<int> getHelper(AVLNode *node, const string &key) const;
+
     int& operator[](const string& key);
+
+    int& overLoadOneHelper(const string& key, AVLNode *node);
 
     vector<string> findRange(string lowKey, string highKey) const;
 
+    void findRangeHelper(AVLNode *node, AVLNodeVisitor& visitor, string lowKey, string highKey) const;
+
     vector<string> keys() const;
+
+    void keysHelper(AVLNode *node, AVLNodeVisitor& visitor) const;
 
     size_t size() const;
 
@@ -78,9 +127,20 @@ public:
 
     //ostream& operator<<(ostream& os, const AVLTree & avltree) const;
 
-    AVLTree(const AVLTree& other);
+    //AVLTree(const AVLTree& other);
 
     //void operator=(const AVLTree& other);
+
+    void leftRotate(AVLNode*& current) {
+
+    }
+
+    void rightRotate(AVLNode*& current) {
+        AVLNode* problemNode;
+        
+
+    }
+
 
 };
 
